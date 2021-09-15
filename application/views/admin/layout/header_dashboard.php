eca404eaffba7010fa1682216ddeeed1149e9d1e
smwebprof <smwebprof@gmail.com>
2021-09-15 11:32:30 +0530

    dashboard changes 06/09/2021

--- application/views/admin/layout/header_dashboard.php
+++ application/views/admin/layout/header_dashboard.php
@@ -23,7 +23,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <meta content="width=device-width, initial-scale=1" name="viewport"/>
 <meta content="" name="description"/>
 <meta content="" name="author"/>
-<meta http-equiv="refresh" content="60">
+<meta http-equiv="refresh" content="600">
 <link rel="shortcut icon" href="<?php echo ASSETS_PATH; ?>img/favicon_agrimin.png">
 <!-- BEGIN GLOBAL MANDATORY STYLES -->
 <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
